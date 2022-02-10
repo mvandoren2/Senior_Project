43e050ea3995244505b36b0b7be2052712918079
AlexAlestra100 <58407966+AlexAlestra100@users.noreply.github.com>
2022-02-09 20:44:02 -0800

    update

--- presales/management/views.py
+++ presales/management/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_list_or_404
 from .models import *
 from django.contrib.auth.decorators import login_required
 
+#make login page a tab
+
 @login_required
 def dashboard(request):
    #pull all existing appiontments
