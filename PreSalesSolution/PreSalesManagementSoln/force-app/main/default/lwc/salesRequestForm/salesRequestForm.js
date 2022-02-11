56ec2606533a75fa9af882398fd9f21a084aafe7
Timoty Thao <thaotimothy604@gamil.com>
2022-02-11 14:33:32 -0800

    flying donut #146 redo

--- PreSalesSolution/PreSalesManagementSoln/force-app/main/default/lwc/salesRequestForm/salesRequestForm.js
+++ PreSalesSolution/PreSalesManagementSoln/force-app/main/default/lwc/salesRequestForm/salesRequestForm.js
@@ -1,3 +1,21 @@
 import { LightningElement } from 'lwc';
 
-export default class SalesRequestForm extends LightningElement {}
+export default class SalesRequestForm extends LightningElement {
+    value = ' ';
+
+    get options() {
+        return [
+            { label: 'Opportunity #1', value: '1234445' },
+            { label: 'Opportunity #2', value: '3455567' },
+            { label: 'Opportunity #3', value: '8133456' },
+            { label: 'Opportunity #4', value: '8139080' },
+            { label: 'Opportunity #5', value: '8567890' },
+            { label: 'Opportunity #6', value: '9178889' },
+            { label: 'Opportunity #7', value: '9808891' },
+        ];
+    }
+
+    handleChange(event) {
+        this.value = event.detail.value;
+    }
+}
