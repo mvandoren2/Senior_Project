5d9594e6c18164036b96cf2666eba3fd09058bf2
yeyintnayhtet <yeyintyummy@gmail.com>
2022-02-13 18:38:46 -0800

    flying donut #137,138,139,140

--- PreSalesSolution/PreSalesManagementSoln/force-app/main/default/lwc/preSalesTeam/preSalesTeam.js
+++ PreSalesSolution/PreSalesManagementSoln/force-app/main/default/lwc/preSalesTeam/preSalesTeam.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
 
-export default class PreSalesTeam extends LightningElement {}
+export default class PreSalesTeam extends LightningElement {
+
+    taskType = "Task Type";
+    timeInfo = "HH:MM AM/PM Day MM/DD/YY";
+
+
+    clientInfo = "Company Inc,";
+    productInfo = "Software 2.2";
+    meetingInfo = "Zoom link/Meeting location";
+    salesTeamInfo = "Team Member A, Team Member B";
+    meetingNotesInfo = "Sales team member notes which are relevant to the pre-sales team member's task. Notes may be added by the pre-sales manager when the request is assigned.";
+}
