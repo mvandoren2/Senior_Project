c5d8bbc74819a9bde0a3507d95748e1199c57604
AlexAlestra100 <58407966+AlexAlestra100@users.noreply.github.com>
2022-02-19 17:09:30 -0800

    Completed task 130 on flying donut
    I made the database and remade the requirements2.txt so if you have a python venv then use that file.

--- presales/management/admin.py
+++ presales/management/admin.py
@@ -3,12 +3,18 @@ from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import *
 
-class ProspectAdmin(admin.ModelAdmin):
-   filter_horizontal = ('products',)
+class ProductandMemberSel(admin.ModelAdmin):
+   filter_horizontal = ('products','members',)
+
+class dateTime(admin.ModelAdmin):
+   readonly_fields = ('date_time',)
 
 admin.site.site_header = 'Pre-Sales Management Administration'
 admin.site.site_title = 'Pre-Sales Management Administration'
 admin.site.index_title = 'Welcome Pre-Sales Management Administrators'
+admin.site.register(StatusHistory, dateTime)
 admin.site.register(Product)
-admin.site.register(Appointment, ProspectAdmin)
+admin.site.register(UserRole)
+admin.site.register(PresalesMember)
+admin.site.register(Activity, ProductandMemberSel)
 admin.site.unregister(Group)
--- presales/management/models.py
+++ presales/management/models.py
@@ -1,26 +1,65 @@
 from django.db import models
 
-class Product(models.Model):
-   product_ID = models.AutoField(primary_key=True)
-   name = models.CharField(max_length=100)
+class StatusHistory(models.Model):
+   status_history_ID = models.AutoField(primary_key=True)
+   activity_ID = models.ForeignKey('Activity', on_delete=models.CASCADE)
+   status = models.CharField(max_length=50)
+   date_time = models.DateTimeField(auto_now_add=True)
+
+   class Meta:
+      verbose_name = 'Status History'
+      verbose_name_plural = 'Status History'
+
+   def __str__(self):
+      return str(self.status_history_ID)
+
+class UserRole(models.Model):
+   roles_ID = models.AutoField(primary_key=True)
+   name = models.CharField(max_length=50)
+
+   class meta:
+      verbose_name_plural = 'User Roles'
 
    def __str__(self):
       return self.name
 
-class Appointment(models.Model):
-   appointment_ID = models.AutoField(primary_key=True)
-   opportunityName = models.CharField(max_length=100)
-   opportunity_ID = models.CharField(max_length=100)
-   account_ID = models.CharField(max_length=100)
-   salesmemeber = models.CharField(max_length=100)
-   oneDate = models.DateField(blank=True, null=True, help_text = "Year-Month-Day")
-   oneTime = models.TimeField(blank=True, null=True, help_text = "Hour:Minute:Second")
-   twoDate = models.DateField(blank=True, null=True, help_text = "Year-Month-Day")
-   twoTime = models.TimeField(blank=True, null=True, help_text = "Hour:Minute:Second")
-   threeDate = models.DateField(blank=True, null=True, help_text = "Year-Month-Day")
-   threeTime = models.TimeField(blank=True, null=True, help_text = "Hour:Minute:Second")
-   description = models.CharField(blank=True, null=True, max_length=500)
-   products = models.ManyToManyField(Product)
+class Product(models.Model):
+   product_ID = models.AutoField(primary_key=True)
+   external_product_ID = models.CharField(max_length=50, blank=True, null=True)
+   name = models.CharField(max_length=100)
+
+   class meta:
+      verbose_name = 'Product'
+      verbose_name_plural = 'Products'
 
    def __str__(self):
-      return self.opportunityName
+      return self.name
+
+class PresalesMember(models.Model):
+   presales_member_ID = models.AutoField(primary_key=True)
+   external_presales_member_ID = models.CharField(max_length=50)
+   user_role = models.ForeignKey(UserRole, on_delete=models.CASCADE)
+
+   class meta:
+      verbose_name = 'Presales Member'
+      verbose_name_plural = 'Presales Members'
+
+   def __str__(self):
+      return str(self.presales_member_ID)
+
+class Activity(models.Model):
+   activity_ID = models.AutoField(primary_key=True)
+   opportunity_ID = models.CharField(max_length=100)
+   members = models.ManyToManyField(PresalesMember)
+   oneDateTime = models.DateTimeField(blank=True, null=True, help_text = "Year-Month-Day Hour:Minute:Second")
+   twoDateTime = models.DateTimeField(blank=True, null=True, help_text = "Year-Month-Day Hour:Minute:Second")
+   threeDateTime = models.DateTimeField(blank=True, null=True, help_text = "Year-Month-Day Hour:Minute:Second")
+   selectedDateTime = models.DateTimeField(blank=True, null=True, help_text = "Year-Month-Day Hour:Minute:Second")
+   products = models.ManyToManyField(Product)
+   description = models.CharField(blank=True, null=True, max_length=500)
+
+   class meta:
+      verbose_name_plural = 'Activities'
+
+   def __str__(self):
+      return str(self.activity_ID)
