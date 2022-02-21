d6cd44725a8d9c20728c7a72dc0f5bbc9b15c790
AlexAlestra100 <58407966+AlexAlestra100@users.noreply.github.com>
2022-02-20 16:44:15 -0800

    Finished Task #198 and also removed unnecessary file
    I have finished making an api endpoint to receive json raw data on the back end.

--- presales/management/views.py
+++ presales/management/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_list_or_404
 from .models import *
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-#make login page a tab
-
+@csrf_exempt
 @login_required
 def dashboard(request):
-   #pull all existing appiontments
-   myUser = Appointment.objects.all()
+   myUser = Activity.objects.all()
    print(myUser)
+
+   if(request.method == 'POST'):
+      activity = json.loads(request.body)
+      print(activity["address"]["city"])
+
    return render(request, 'management/dashboard.html', {'myUser': myUser})
