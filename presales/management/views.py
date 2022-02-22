57e494ad396424adeeb63dbe4834364b9a6fcffa
AlexAlestra100 <58407966+AlexAlestra100@users.noreply.github.com>
2022-02-21 16:52:58 -0800

    Task #213
    I have finished making an api that will dump all of the contents of the activity table and send it to receiver once pinged.

--- presales/management/views.py
+++ presales/management/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_list_or_404
 from .models import *
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 import json
 
 @csrf_exempt
@@ -12,6 +13,16 @@ def dashboard(request):
 
    if(request.method == 'POST'):
       activity = json.loads(request.body)
-      print(activity["address"]["city"])
+      print(activity)
+
+   if(request.method == 'GET'):
+      activity = list(Activity.objects.values())
+      if(activity):
+         activity_json = json.dumps(activity, indent=4, sort_keys=True, default=str)
+         print(activity_json)
+      else:
+         activity_json = [{'error': 'No activities found'}]
+      
+      return HttpResponse(activity_json, content_type='application/json')
 
    return render(request, 'management/dashboard.html', {'myUser': myUser})
