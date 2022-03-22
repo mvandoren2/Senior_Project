672ff0cded8801d93020a2b2d2fc4e4d9c2247dc
Kendall Helms <kchelms95@gmail.com>
2022-03-22 16:13:39 -0700

    Refactor ActivityListView, more OOP, more better

--- PreSalesSolution/PreSalesManagementSoln/force-app/main/default/lwc/activityListView/activityListView.js
+++ PreSalesSolution/PreSalesManagementSoln/force-app/main/default/lwc/activityListView/activityListView.js
@@ -1,5 +1,9 @@
 import { LightningElement } from 'lwc';
+import { ColumnSortHandler } from './columnSortHandler';
+import { RowActionHandler } from './rowActionHandler';
+import { TableFilterHandler } from './tableFilterHandler';
 import { TableFormatter } from './tableFormatter';
+
 
 export default class ActivityListView extends LightningElement {
     constructor() {
@@ -8,29 +12,19 @@ export default class ActivityListView extends LightningElement {
         this.updateData()
     }
 
-    
-    profileNum = 1
-    formatNum = 1
-    
-    userProfile = {
-        PSMan: this.profileNum === 1, 
-        PSTeam: this.profileNum === 2,
-        Sales: this.profileNum === 3
-    }
-
-    format = {
-        large: this.formatNum === 1,
-        medium: this.formatNum === 2,
-        small: this.formatNum === 3
-    }
-
     tableFormatter = new TableFormatter()
+    columnSortHandler = new ColumnSortHandler(this)
+    tableFilterHandler = new TableFilterHandler(this)
+    rowActionHandler = new RowActionHandler(this)
+
 
     tableData = this.tableFormatter.getEmptyTableFormat()
     columns = this.tableData.columns
     actions = this.tableData.actions
-    data = this.tableData.data.dislpay
+    rows = this.tableData.rows.dislpay
+    displayedRows = [...this.rows]
 
+    
     updateData = () => {
         this.tableFormatter.getTableFormat()
             .then(data => this.fillTable(data))
@@ -41,95 +35,8 @@ export default class ActivityListView extends LightningElement {
 
         this.columns = this.tableData.columns
         this.actions = this.tableData.actions
-        this.data = this.tableData.data.dislpay
+        this.rows = this.tableData.rows.dislpay
 
-        this.filteredData = [...this.data]
-    }
-
-    calculateRowWidth = (component, evt) => {
-        console.log(component, evt)  
-    }
-
-    setNewWidth = (evt) => {
-        console.log(evt)
-    }
-
-    flipShowAssign = () => {
-        this.template.querySelector('c-assign-team-modal').toggleShow(this.rowID)
-    }
-
-    flipShowDecline = () => {
-        this.template.querySelector('c-status-change-modal').toggleShow(this.rowID)
-    }
-
-    filters = []
-    filteredData = [...this.data]
-
-    sortAsc = true
-    sortedBy
-
-    sortBy = (field, reverse, primer) => {
-        const key = (x) => {
-            const val = Array.isArray(x[field]) ? x[field][0] : x[field]
-            
-            return primer ? primer(val) : val
-        };
-    
-        return function (a, b) {
-            a = key(a)
-            b = key(b)
-            return reverse * ((a > b) - (b > a))
-        };
-    }
-
-    handleSort = (evt) => {
-        let sortedBy = evt.target.dataset.item
-
-        if(sortedBy == 'description') return
-
-        if(this.sortedBy == sortedBy)
-            this.sortAsc = !this.sortAsc
-
-        else this.sortAsc = true
-
-        let cloneData = [...this.filteredData]
-        cloneData.sort(this.sortBy(sortedBy, this.sortAsc ? 1 : -1))
-        this.filteredData = cloneData
-
-        this.sortedBy = sortedBy
-    }
-
-    handleFilter = (evt) => {
-        const filterField = evt.target.dataset.item
-
-        const query = evt.target.value
-
-        this.filters = this.filters.filter(item => item.field != filterField)
-        
-        if(query !== '')
-            this.filters = this.filters.concat({field: filterField, query: query})
-
-        this.filteredData = [...this.data]
-        
-        this.filters.forEach(filter => {
-            this.filteredData = this.filteredData
-                .filter(row => (
-                    row[filter.field].toLowerCase().includes(filter.query.toLowerCase())
-                ))
-        })
-    }
-
-    rowID
-
-    handleMenuAction = (evt) => {
-        const parentElement = evt.target.closest('tr')
-
-        this.rowID = parentElement.dataset.item
-
-        if (evt.target.dataset.item == 'assign_request')
-            this.flipShowAssign()
-
-        if(evt.target.dataset.item == 'decline_request')
-            this.flipShowDecline()   
+        this.displayedRows = [...this.rows]
     }
 }
---
+++ PreSalesSolution/PreSalesManagementSoln/force-app/main/default/lwc/activityListView/tableFilterHandler.js
@@ -0,0 +1,35 @@
+import ActivityListView from "./activityListView"
+
+export class TableFilterHandler {
+    constructor(parentObj) {
+        this.parent = parentObj
+
+        if(!(this.parent instanceof ActivityListView))
+            throw new Error('TableFilterHandler was invoked by an invalid parent')        
+    }
+
+    filters = []
+
+    handleFilter = (evt) => {
+        const filterField = evt.target.dataset.item
+
+        const query = evt.target.value
+
+        this.filters = this.filters.filter(item => item.field !== filterField)
+        
+        if(query !== '')
+            this.filters = this.filters.concat({field: filterField, query: query})
+
+        let rows = [...this.parent.rows]
+        
+        this.filters.forEach(filter => {
+            rows = rows
+                .filter(row => (
+                    row[filter.field].toLowerCase().includes(filter.query.toLowerCase())
+                ))
+        })
+
+        this.parent.displayedRows = rows
+    }
+
+}
