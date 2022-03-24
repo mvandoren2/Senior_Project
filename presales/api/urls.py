2c9d360796abbc6e898ca1aca809e3f6020d0630
Kendall Helms <kchelms95@gmail.com>
2022-03-24 08:16:19 -0700

    Spell check

--- presales/api/urls.py
+++ presales/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('add_members_date/', views.addMembersandDate),
     path('activity/', views.getActivity),
-    path('activitys/', views.getActivitys),
+    path('activities/', views.getActivitys),
     path('activity/active/', views.getActiveActvivitys),
     path('activity/request/', views.getRequestActivitys),
     path('products/', views.getProducts),
