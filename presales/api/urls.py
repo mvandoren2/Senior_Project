a24db873a17a706114d1ba70df85d69764afdafd
AlexAlestra100 <58407966+AlexAlestra100@users.noreply.github.com>
2022-03-24 12:47:03 -0700

    completes activities api functionality

--- presales/api/urls.py
+++ presales/api/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('add_members_date/', views.addMembersandDate),
     path('activity/', views.getActivity),
-    path('activities/', views.getActivitys),
-    path('activity/active/', views.getActiveActvivitys),
-    path('activity/request/', views.getRequestActivitys),
+    path('activities/', views.getActivities),
+    path('activity/active/', views.getActiveActvivities),
+    path('activity/request/', views.getRequestActivities),
     path('products/', views.getProducts),
     path('member/<id>/', views.getMember),
     path('members/', views.getMembers),
