aafd562a89417c396df8ec8bfdee59d63fce0b72
anujregmi1 <anujregmi77@gmail.com>
2022-03-26 13:54:47 -0700

    presales manager can now add notes to a declined activity

--- PreSalesSolution/PreSalesManagementSoln/force-app/main/default/lwc/statusChangeModal/statusChangeModal.js
+++ PreSalesSolution/PreSalesManagementSoln/force-app/main/default/lwc/statusChangeModal/statusChangeModal.js
@@ -1,8 +1,12 @@
 import { LightningElement,api,track } from 'lwc';
+import uId from '@salesforce/user/Id';
 
 export default class StatusChangeModal extends LightningElement {
 
     activityID;
+    userId = uId;
+    declineMessage;
+    @track submitStatus = true;
 
     @api toggleShow = (rowID) => {
         this.boxClasses = this.boxClasses.includes('slds-fade-in-open') ? 'slds-modal' : 'slds-modal slds-fade-in-open'
@@ -14,9 +18,14 @@ export default class StatusChangeModal extends LightningElement {
     @track boxClasses = 'slds-modal'
     @track backdropClasses = 'slds-backdrop'
 
+    setDisableButton(){
+        this.declineMessage = this.template.querySelector("lightning-textarea").value;
+        this.submitStatus = !(this.declineMessage !== '');
+    }
+
     //push the data to backend
     declineActivity(){
-        
+
         let activity_ID = this.activityID.activity_ID;
 
         //create a json to push
@@ -24,7 +33,6 @@ export default class StatusChangeModal extends LightningElement {
             'status': "Decline"
         };
 
-        console.log(pushingData);
         fetch('http://localhost:8080/api/activity/' + activity_ID + '/', {
             method: 'PATCH', 
             headers: {
@@ -35,5 +43,24 @@ export default class StatusChangeModal extends LightningElement {
         .catch((error) => {
             console.error('Error:', error);
         });
+
+        let notesData = {
+            'member': this.userId,
+            'note_text': this.declineMessage
+        }
+
+        fetch('http://localhost:8080/api/activity/' + activity_ID + '/notes/', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(notesData),
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+        this.template.querySelector("lightning-textarea").value = "";
+        this.toggleShow();
     }
 }
