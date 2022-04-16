6c17f9e1be26172678d941ab375bfc78a0b0679c
Timoty Thao <thaotimothy604@gamil.com>
2022-04-15 23:27:56 -0700

    Complete Task #279

--- presales/settings/base.py
+++ presales/settings/base.py
@@ -40,7 +40,18 @@ INSTALLED_APPS = [
     'django_salesforce_oauth',
     'corsheaders',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
+
+
+REST_FRAMEWORK = {  
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+        'rest_framework.permissions.IsAuthenticated',
+   ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
