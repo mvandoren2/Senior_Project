7afc410a0dbbeb79918de744e59698809c8f645b
Kendall Helms <kchelms95@gmail.com>
2022-04-26 18:58:02 -0700

    Created note view modal

---
+++ PreSalesSolution/PreSalesManagementSoln/force-app/main/default/lwc/noteModal/noteModal.js
@@ -0,0 +1,32 @@
+import { LightningElement, api } from 'lwc';
+
+export default class NoteModal extends LightningElement {   
+    @api showModal = (activity) => {
+        this.notesView = this.template.querySelector('c-notes-view')
+
+        this.activityID = activity.activity_ID
+        
+        this.notesView.setAttribute('data-activity-id', this.activityID)
+        
+        this.toggleModalClasses()
+
+        this.notesView.init()
+    }
+
+    closeModal = () => {
+        this.toggleModalClasses()
+
+        this.notesView.reset()
+    }
+    
+    boxClasses = 'slds-modal'
+    backdropClasses = 'slds-backdrop'
+
+    toggleModalClasses = () => {
+        this.boxClasses = this.boxClasses.includes('slds-fade-in-open') ? 
+            'slds-modal' : 'slds-modal slds-fade-in-open'
+            
+        this.backdropClasses = this.backdropClasses.includes('slds-backdrop_open') ? 
+            'slds-backdrop' : 'slds-backdrop slds-backdrop_open'
+    }
+}
