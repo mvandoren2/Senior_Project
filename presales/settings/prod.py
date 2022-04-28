e69b8470f99bdbfe51018d4a9d98560d18a298a2
AlexAlestra100 <58407966+AlexAlestra100@users.noreply.github.com>
2022-04-27 21:45:33 -0700

    host update

--- presales/settings/prod.py
+++ presales/settings/prod.py
@@ -13,12 +13,12 @@ except:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
     # HTTPS SETTINGS
-    SECURE_SSL_REDIRECT = False
-    CSRF_COOKIE_SECURE = False
-    SESSION_COOKIE_SECURE = False
+    SECURE_SSL_REDIRECT = True
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
 
     #will be True when we have SSL certificate
-    SECURE_SSL_REDIRECT = False
+    SECURE_SSL_REDIRECT = True
 
     # Maybes
     # HSST SETTINGS
